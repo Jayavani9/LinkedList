4b94481fea768f1c2d89da3dc9f0136fd23cc2c8
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-03 22:14:00 -0700

    Update Add Two NumbersLL.java

--- Add Two NumbersLL.java
+++ Add Two NumbersLL.java
@@ -15,4 +15,40 @@ Output: [0]
 Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // Tc: O(m+n) and Tc: O(1)
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null)
+        {
+            int sum = carry;
+            if(l1 != null) {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+            carry = sum/10;
+            cur.next = new ListNode(sum%10);
+            cur = cur.next;
+        }
+        if(carry > 0) cur.next = new ListNode(carry);
+        return dummy.next;
+        
+    }
+}
  
