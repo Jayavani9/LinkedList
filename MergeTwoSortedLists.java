f8bc4a9ea2438aef6c0b365c346198fd7d1aa303
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-03 22:52:53 -0700

    Update MergeTwoSortedLists.java

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -13,5 +13,38 @@ Output: []
 Input: list1 = [], list2 = [0]
 Output: [0]
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //Tc: O(m+n) and Sc: O(1)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode ans = new ListNode(0);
+        ListNode cur = ans;
+        while(list1 != null && list2 != null)
+        {
+            if(list1.val < list2.val)
+            {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                cur.next = list2;
+                list2= list2.next;
+            }
+            cur = cur.next;
+        }
+        if(list1 != null) cur.next = list1;
+        else cur.next = list2;
 
+        return ans.next;
+}
+}
 
