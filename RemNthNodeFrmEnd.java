b442a3ebdc5e3010f6be896117565ad69c6f5d7f
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2024-01-04 06:05:16 -0800

    Update RemNthNodeFrmEnd.java

--- RemNthNodeFrmEnd.java
+++ RemNthNodeFrmEnd.java
@@ -23,6 +23,51 @@ Output: [1]
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+1. Two Pass solution
+  
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        //Tc: O(n) Sc: O(1)
+        int len = 0;
+        ListNode first = head;
+        while(first != null)
+        {
+            len++;
+            first = first.next;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        len = len - n;
+        first = dummy;
+        while(len > 0)
+        {
+            len--;
+            first = first.next;
+        }
+
+        first.next = first.next.next;
+        return dummy.next;
+        
+    }
+}
+
+
+2. One pass solution
+  
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         //Tc: O(L)  Sc: O(1)
