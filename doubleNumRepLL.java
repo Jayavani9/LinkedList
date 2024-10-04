a033f80efb718c07ba2ba84b55f65acad233cf38
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-10-03 18:11:34 -0700

    Update doubleNumRepLL.java

--- doubleNumRepLL.java
+++ doubleNumRepLL.java
@@ -12,6 +12,65 @@
  * }
  */
 
+
+//In place
+class Solution {
+    //Tc: O(n) Sc: O(1) 
+    public ListNode doubleIt(ListNode head) {
+       if (head.val > 4) {
+            head = new ListNode(0, head);
+        }
+
+        ListNode node = head;
+        while (node != null) {
+            node.val = (node.val * 2) % 10;  
+
+            if (node.next != null && node.next.val > 4) {
+                node.val++;  
+            }
+            
+            node = node.next;  
+        }
+        
+        return head;
+
+        
+    }
+}
+
+
+
+//Using a Stack
+//Tc: O(n)  Sc: O(n) 
+ Stack<ListNode> st = new Stack<>();
+        ListNode cur = head;
+
+        while(cur != null)
+        {
+            st.push(cur);
+            cur = cur.next;
+        }
+
+        int carry = 0;
+        while(!st.isEmpty())
+        {
+            cur = st.pop();
+            int value = cur.val*2 + carry;
+            cur.val = value%10;
+            carry = value/10;
+        }
+
+        if(carry != 0)
+        {
+            ListNode res = new ListNode(carry);
+            res.next = head;
+            head = res;
+        }
+        return head;
+    }
+}
+
+
 //Using Reverse 
 //Tc: O(n) Sc: O(1) 
 class Solution {
